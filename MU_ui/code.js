e5b0c747f21f3ebb3bc6c4c1b1355b655f6149e4
Dmitrii Smirnov <dismirow@gmail.com>
2024-08-04 15:22:26 +0300

    add changer types of MU

--- MU_ui/code.js
+++ MU_ui/code.js
@@ -1,7 +1,22 @@
 // Add active class to the current button (highlight it)
 var header = document.getElementById("buttons");
 var btns = header.getElementsByClassName("btn-outline-primary");
+const typeMU = document.getElementById("typeMU")
 
+typeMU.addEventListener("click", function() {
+  btns = document.getElementById("buttons").getElementsByClassName("btn-outline-primary");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].classList.remove('active')
+  } 
+  typeMU.innerText = (this.innerText === 'Цифровой') ? 'Буквенный': 'Цифровой'
+
+
+  const hiddenButtons = document.getElementsByClassName("hidden")
+    for (var i = 0; i < hiddenButtons.length; i++) {
+      hiddenButtons[i].classList.toggle('d-none')
+    } 
+
+});
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     // var current = document.getElementsByClassName("active");
@@ -9,5 +24,6 @@ for (var i = 0; i < btns.length; i++) {
     // this.className += " active";
     // console.log(this)
     this.classList.toggle('active')
+    // console.log(this)
   });
 }
