4709640727100cbb8698c69e013266d6192df85b
Dmitrii Smirnov <dismirow@gmail.com>
2024-10-02 13:24:20 +0300

    add_modal_fields

--- battery/assets/js/modal.js
+++ battery/assets/js/modal.js
@@ -9,7 +9,7 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
   //
   // Update the modal's content.
 //   var modalTitle = exampleModal.querySelector('.modal-title')
-  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+  var modalBodyInput = exampleModal.getElementById('count')
 
 //   modalTitle.textContent = 'New message to ' + recipient
   modalBodyInput.value = recipient
