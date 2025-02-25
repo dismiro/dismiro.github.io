30be78d0df6eaedaaaedf28e32c63cd4a24e9c03
Dmitrii Smirnov <dismirow@gmail.com>
2025-02-26 00:05:05 +0300

    add_accordion

--- specification/code.js
+++ specification/code.js
@@ -8,58 +8,101 @@
 // }
 
 async function handleFileAsync(e) {
-    const file = e.target.files[0];
+    // console.log(e.target.files)
+    var dataJS = []
+    for (let file of e.target.files){
+
+    // const file = e.target.files[0];
     const data = await file.arrayBuffer();
     /* data is an ArrayBuffer */
     const workbook = XLSX.read(data);
     var  wsnames  =  workbook.SheetNames ;
-    // console.log(wsnames)
-    var  jsa  =  XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+    for (let sheetName of wsnames){
+        const  dataFromOneFile  =  XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]).
+        map((item) => {
+            const value = item['Значение']
+            .replace(/\(.\)/g,'')
+            .replace(/\(..\)/g,'')
+            .replace('x','х')
+            .replace(' ','')
+
+            const length = value.substring(0,value.indexOf('-'))
+            const type = value.substring(value.indexOf('-') + 1, value.length)
+            const count = item['Количество']
+            return {'row' : item['__rowNum__'],'value':value,'type':type, 'length':length, 'count':count}
+})
+dataJS.push(dataFromOneFile)
+const newDiv = document.createElement("div");
+const title = document.createElement("h3");
+const button = document.createElement("button");
+button.innerHTML= 'text'
+button.setAttribute('type',"button")
+button.setAttribute('data-bs-toggle',"collapse")
+button.setAttribute('data-bs-target',"#collapseOne")
+button.setAttribute('aria-expanded',"true")
+button.setAttribute('aria-controls',"collapseOne")
+
+newDiv.classList.add('accordion-item')
+title.classList.add("accordion-header")
+button.classList.add("accordion-button")
+title.id = 'headingOne'
+title.appendChild(button)
+newDiv.appendChild(title)
+newDiv.innerHTML += `<div class="accordion-collapse collapse show" id="collapseOne" aria-labelledby="headingOne" data-bs-parent="#accordionDefault">
+  <div class="accordion-body">This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element.</div>
+</div>`
+document.getElementById('accordionExample').appendChild(newDiv)
+}
+   
+    
+    ;
     // var  html  =  XLSX.utils.sheet_to_html (workbook.Sheets[wsnames],{editable:true}).replace("<table",'<table id="data-table" class="table table-sm"') ;
     // var  container  =  document.getElementById ("table")
     // container.innerHTML = html
     // const dataJS = jsa
-    const dataJS = jsa.map((item) => {
-        const value = item['Значение']
-        .replace(/\(.\)/g,'')
-        .replace(/\(..\)/g,'')
-        .replace('x','х')
-        .replace(' ','')
-
-        const length = value.substring(0,value.indexOf('-'))
-        const type = value.substring(value.indexOf('-') + 1, value.length)
-        const count = item['Количество']
-        return {'row' : item['__rowNum__'],'value':value,'type':type, 'length':length, 'count':count}
-})
+    }
     console.log(dataJS)
+//     const dataJS = jsa.map((item) => {
+//         const value = item['Значение']
+//         .replace(/\(.\)/g,'')
+//         .replace(/\(..\)/g,'')
+//         .replace('x','х')
+//         .replace(' ','')
+
+//         const length = value.substring(0,value.indexOf('-'))
+//         const type = value.substring(value.indexOf('-') + 1, value.length)
+//         const count = item['Количество']
+//         return {'row' : item['__rowNum__'],'value':value,'type':type, 'length':length, 'count':count}
+// })
+    // console.log(dataJS)
     const table = document.createElement('table');
 
 // Заполнение таблицы данными
-    dataJS.forEach(item => {
-    const row = table.insertRow();
-     Object.values(item).forEach(text => {
-    const cell = row.insertCell();
-    cell.textContent = text;
- });
-});
-var outData = dataJS.reduce((acc,item)=> {
-    const currentType = item['type'].replace(' ','')
-    const currentLength = item['length'] * item['count']
-    if (acc[currentType]){
-        acc[currentType] = acc[currentType] + currentLength
-        return acc
-    }
-    acc[currentType] = currentLength
-    return acc
-}, {})
-console.log(outData)
+//     dataJS.forEach(item => {
+//     const row = table.insertRow();
+//      Object.values(item).forEach(text => {
+//     const cell = row.insertCell();
+//     cell.textContent = text;
+//  });
+// });
+// var outData = dataJS.reduce((acc,item)=> {
+//     const currentType = item['type'].replace(' ','')
+//     const currentLength = item['length'] * item['count']
+//     if (acc[currentType]){
+//         acc[currentType] = acc[currentType] + currentLength
+//         return acc
+//     }
+//     acc[currentType] = currentLength
+//     return acc
+// }, {})
+// console.log(outData)
 table.classList.add('table')
 table.classList.add('table-sm')
 const processedData = document.getElementById('processedData') 
 processedData.appendChild(table);
-    const out = document.getElementById('outputResult') 
-    var listTypes = Object.entries(outData) 
-    out.innerHTML = listTypes.map((item) => `<li>${item[0]} ---- ${item[1]} </li>`).join(' ');    
+    // const out = document.getElementById('outputResult') 
+    // var listTypes = Object.entries(outData) 
+    // out.innerHTML = listTypes.map((item) => `<li>${item[0]} ---- ${item[1]} </li>`).join(' ');    
 
     // var  jsa  =  XLSX.utils.sheet_to_json ( worksheet , opts ) ;​​​​ 
     // console.log(workbook)
